66ab197124c04c88d3d665c9b38fe561c74effa0
Tran Thanh Nhut <tranthanhnhut4708@gmail.com>
2026-06-18 23:32:48 +0700

    Add Program class with properties for first name, last name, age, and phone number

---
+++ C#_BASIC/8.cs
@@ -0,0 +1,18 @@
+using System;
+namespace BaiTap;
+
+class Program
+{
+    private String firstName;
+    private String lastName;
+    private int old;
+
+    private String phoneNumber;
+
+    public string FirstName { get => firstName; set => firstName = value; }
+    public string LastName { get => lastName; set => lastName = value; }
+    public int Old { get => old; set => old = value; }
+    public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+
+}
