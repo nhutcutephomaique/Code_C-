9bb9806b55b9108dec483c4acaac726366a154a9
Tran Thanh Nhut <tranthanhnhut4708@gmail.com>
2026-06-20 23:14:02 +0700

    Add BubbleSort class with implementation of the Bubble Sort algorithm

---
+++ C#_BASIC/10.cs
@@ -0,0 +1,28 @@
+using System;
+namespace DSA;
+
+class BubbleSort
+{
+
+
+    static void BubbleSortAlgorithm(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+}
