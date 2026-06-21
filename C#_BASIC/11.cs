21839acde71417d581446b3b593b2f9da29f1948
Tran Thanh Nhut <tranthanhnhut4708@gmail.com>
2026-06-21 22:41:22 +0700

    Add SelectionSort class with implementation of the Selection Sort algorithm

---
+++ C#_BASIC/11.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace CSharpBasic
+{
+    class SelectionSort
+    {
+        static void SelectionSortArray(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+                }
+
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
